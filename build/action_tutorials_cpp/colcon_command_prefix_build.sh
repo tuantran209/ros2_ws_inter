c6e30ebcb22cdf0000f8d5b2d373db0a0bf229f5
tuantran209 <tuan20092001ak@gmail.com>
2024-01-29 16:10:29 +0700

    server and client fibonacci

---
+++ build/action_tutorials_cpp/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/tuantran/ros2_ws_int/install/action_tutorials_interfaces/share/action_tutorials_interfaces/package.sh"
---
+++ build/action_tutorials_interfaces/action_tutorials_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tuantran/ros2_ws_int/build/action_tutorials_interfaces/action_tutorials_interfaces__py
+# Build directory: /home/tuantran/ros2_ws_int/build/action_tutorials_interfaces/action_tutorials_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
